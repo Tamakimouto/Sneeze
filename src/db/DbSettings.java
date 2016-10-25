199905314132f314798693d829277dab274305ce
Anthony Zheng <Anthony@fopen-dream.space>
2016-10-24 22:59:22 -0400

    edit spacing

--- src/db/DbSettings.java
+++ src/db/DbSettings.java
@@ -1,7 +1,7 @@
 package db;
 
 public abstract class DbSettings {
-	public static final String DB_DRIVE_NAME = "com.mysql.jdbc.Driver";
+    public static final String DB_DRIVE_NAME = "com.mysql.jdbc.Driver";
     public static final String DB_CONNECTION_URL = "jdbc:mysql://localhost:3306/sneeze";
     public static final String DB_CONNECTION_USERNAME = "root";
     public static final String DB_CONNECTION_PASSWORD = "10068366";
