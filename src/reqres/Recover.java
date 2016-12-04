adfd50467e8f91e4aefb7fbc66c2def62cbeabf9
Anthony Zheng <Anthony@fopen-dream.space>
2016-12-04 00:33:19 -0500

    Forgot Password Implementation

--- src/reqres/Recover.java
+++ src/reqres/Recover.java
@@ -8,6 +8,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+
+import db.DbLogic;
+
 /**
  * Servlet implementation class Recover
  */
@@ -26,8 +32,62 @@ public class Recover extends HttpServlet {
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
         String userMail = request.getParameter("mail");
+        String from = "no-reply@mail.com";
+
+        String host = "localhost";
+
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+        Session session = Session.getDefaultInstance(properties);
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(userMail));
+
+            String pass = DbLogic.getUserPass(userMail);
+
+            if (pass != "") {
+                message.setSubject("Sneeze Password");
+                message.setText(pass);
+                Transport.send(message);
+
+                String title = "Send Email";
+                String res = "E-Mail Sent...";
+                String docType =
+                    "<!doctype html public \"-//w3c//dtd html 4.0 " +
+                    "transitional//en\">\n";
+                out.println(docType +
+                        "<html>\n" +
+                        "<head><title>" + title + "</title></head>\n" +
+                        "<body bgcolor=\"#f0f0f0\">\n" +
+                        "<h1 align=\"center\">" + title + "</h1>\n" +
+                        "<p align=\"center\">" + res + "</p>\n" +
+                        "</body></html>");
+            } else {
+                String title = "Send Email";
+                String res = "No account associated with this E-mail";
+                String docType =
+                    "<!doctype html public \"-//w3c//dtd html 4.0 " +
+                    "transitional//en\">\n";
+                out.println(docType +
+                        "<html>\n" +
+                        "<head><title>" + title + "</title></head>\n" +
+                        "<body bgcolor=\"#f0f0f0\">\n" +
+                        "<h1 align=\"center\">" + title + "</h1>\n" +
+                        "<p align=\"center\">" + res + "</p>\n" +
+                        "</body></html>");
+            }
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
     }
 
     /**
