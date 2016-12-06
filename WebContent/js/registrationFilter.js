16e7840149aa94fe6efb2ce78e972493c63fbb09
Robinmg <rmguice@gmail.com>
2016-12-06 00:44:44 -0500

    Adding ajax functionality to sign in/sign up

--- WebContent/js/registrationFilter.js
+++ WebContent/js/registrationFilter.js
@@ -1,5 +1,5 @@
 $(function() {
-    $("#user .signInField").on("change", function() {
+    $("#user.signUpField").on('change', function() {
         var username = $(this).val();
         if (username.length > 5) {
             $(".status").html("Checking Availibility...");
@@ -8,13 +8,20 @@ $(function() {
                 url: "Availability",
                 data: "username=" + username,
                 success: function(msg) {
-                    $(".status").ajaxComplete(function(event, request, settings) {
-                        $(".status").html(msg);
-                    });
+                	if(msg == "This username is taken"){
+                		$("#signUpButton").addClass("disabled");
+                		$("#signUpButton").prop("disabled", true);
+                	}else{
+                		$("#signUpButton").removeClass("disabled");
+                		$("#signUpButton").prop("disabled", false);
+                	}
+                	$(".status").html(msg);
                 }
             });
         } else {
             $(".status").html("Username should be 5 or more characters");
+    		$("#signUpButton").addClass("disabled");
+    		$("#signUpButton").prop("disabled", true);
         }
     });
 });
